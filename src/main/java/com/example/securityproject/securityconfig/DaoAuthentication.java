f0400a06df2fb00986851b230a05b46e8a8b3a31
taron1991 <84153270+taron1991@users.noreply.github.com>
2023-07-15 22:55:38 +0400

    Create DaoAuthentication.java

---
+++ src/main/java/com/example/securityproject/securityconfig/DaoAuthentication.java
@@ -0,0 +1,33 @@
+@EnableWebSecurity
+public class Configure extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sec/admin").hasAuthority("dev:write")
+                .antMatchers("/sec/client").hasAnyAuthority("dev:read","dev:write")
+                .antMatchers("/sec/all").permitAll()
+                .and().formLogin().permitAll();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
+        dao.setPasswordEncoder(passwordEncoder());
+        dao.setUserDetailsService(userDetailsService);
+        return dao;
+    }
+}
